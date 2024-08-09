f688ce1b26ab28eefebe0e9371951467a9ed4a18
azhaannazim <132593214+azhaannazim@users.noreply.github.com>
2024-08-09 17:58:07 +0530

    APIResponse

--- src/main/java/com/ecommerce/project/sbECom/config/AppConstants.java
+++ src/main/java/com/ecommerce/project/sbECom/config/AppConstants.java
@@ -2,5 +2,7 @@ package com.ecommerce.project.sbECom.config;
 
 public class AppConstants {
     public static final String PAGE_NUMBER = "0";
-    public static final String PAGE_SIZE = "10";
+    public static final String PAGE_SIZE = "1";
+    public static final String SORT_CATEGORIES_BY = "categoriesId";
+    public static final String SORT_DIRECTION = "asc";
 }
--- src/main/java/com/ecommerce/project/sbECom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/sbECom/controller/CategoryController.java
@@ -31,8 +31,10 @@ public class CategoryController {
     //it can be used universally
     public ResponseEntity<CategoryResponse> getAllCategories(
             @RequestParam(name = "pageNumber" , defaultValue = AppConstants.PAGE_NUMBER , required = false) Integer pageNumber,
-            @RequestParam(name = "pageSize" , defaultValue = AppConstants.PAGE_SIZE , required = false) Integer pageSize){
-        return new ResponseEntity<>(service.getAllCategories(pageNumber , pageSize) , HttpStatus.OK);
+            @RequestParam(name = "pageSize" , defaultValue = AppConstants.PAGE_SIZE , required = false) Integer pageSize,
+             @RequestParam(name = "sortBy" , defaultValue = AppConstants.SORT_CATEGORIES_BY , required = false) String sortBy,
+            @RequestParam(name = "sortOrder" , defaultValue = AppConstants.SORT_DIRECTION , required = false) String sortOrder){
+        return new ResponseEntity<>(service.getAllCategories(pageNumber , pageSize , sortBy ,sortOrder) , HttpStatus.OK);
     }
 
     @PostMapping("public/categories")
