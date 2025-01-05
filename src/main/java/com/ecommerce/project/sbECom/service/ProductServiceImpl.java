0f837f0d3757d1cfea7ffd2542de1c97d5e73e64
azhaannazim <nazimazhaan17@gmail.com>
2025-01-06 02:48:05 +0530

    fixing pagination && product retrieval

--- src/main/java/com/ecommerce/project/sbECom/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/sbECom/service/ProductServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -68,13 +69,23 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+    public ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder, String keyword, String category) {
         Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
                 ? Sort.by(sortBy).ascending()
                 : Sort.by(sortBy).descending();
 
         Pageable pageDetails = PageRequest.of(pageNumber , pageSize , sortByAndOrder);
-        Page<Product> productsPage = productRepository.findAll(pageDetails);
+        Specification<Product> specs = Specification.where(null);
+        if(keyword != null && !keyword.isEmpty()){
+            specs = specs.and(((root, query, criteriaBuilder) ->
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("productName")) ,"%" + keyword.toLowerCase() + "%")));
+        }
+        if(category != null && !category.isEmpty()){
+            specs = specs.and(((root, query, criteriaBuilder) ->
+                    criteriaBuilder.like(root.get("category").get("categoryName") ,category)));
+        }
+
+        Page<Product> productsPage = productRepository.findAll(specs ,pageDetails);
         List<Product> productList = productsPage.getContent();
 
         if(productList.isEmpty()){
